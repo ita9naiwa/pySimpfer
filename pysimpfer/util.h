adaba2dc0f9a8f9350fdd319aadec72a9a0617cb
Hyunsung Lee <ita9naiwa@gmail.com>
2022-01-30 19:59:41 +0900

    Update util.h

--- pysimpfer/util.h
+++ pysimpfer/util.h
@@ -1,10 +1,6 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-// We need to get the thread number to figure out which RNG to use,
-// but this will fail on OSX etc if we have no openmp enabled compiler.
-// Cython won't let me #ifdef this, so I'm doing it here
-
 #ifdef _OPENMP
 #include <omp.h>
 #endif
